4e87f2a9cb26851e2b7ff686d3bb39ac3bbefb41
shaddy24 <balanday.shaddy@gmail.com>
2019-08-21 15:36:37 +0800

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public SqlConnection con = new SqlConnection(@"Data Source = Balanday-PC\SQLEXPRESS; Initialize Catalog =nissin; Integrated Security = true");
+        static void Main(string[] args)
+        {
+
+            
+
+        }
+
+
+
+
+
+    }
+}
